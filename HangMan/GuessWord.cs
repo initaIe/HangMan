65b24563a52608cd538fdcff5c082e15b4bd6dc8
sergei bagaev <a1f4pvp@yandex.ru>
2024-07-25 22:03:41 +0500

    Add project files.

---
+++ HangMan/GuessWord.cs
@@ -0,0 +1,77 @@
+﻿namespace HangMan
+{
+    public static class GuessWord
+    {
+        private const string _pathToTextFile = "dictionary.txt";
+        private static string? _hiddenWord = String.Empty;
+        public static string? VisibleWord { get; private set; } = String.Empty;
+
+        // Initialization here for better randomness
+        private static Random rnd = new();
+
+        public static bool IsWordSolved
+        {
+            get
+            {
+                return VisibleWord == _hiddenWord;
+            }
+        }
+
+        static GuessWord()
+        {
+
+        }
+
+        public static void Reload()
+        {
+            GenerateHiddenWord();
+            GenerateVisibeWord();
+        }
+
+        public static bool HasLetter(char letter)
+        {
+            return _hiddenWord.Contains(letter);
+        }
+
+        public static void OpenLettersInHiddenWord(char letter)
+        {
+            char[] visibleWordCharArr = VisibleWord.ToCharArray();
+
+            for (int i = 0; i < _hiddenWord.Length; i++)
+            {
+                if (_hiddenWord[i] == letter)
+                {
+                    visibleWordCharArr[i] = letter;
+                }
+            }
+            VisibleWord = new string(visibleWordCharArr);
+        }
+
+        private static void GenerateHiddenWord()
+        {
+            int countLinesInFile = File.ReadLines(_pathToTextFile).Count();
+
+            int minIndex = 0;
+            int maxIndex = countLinesInFile - 1;
+
+            int hiddenWordIndex = rnd.Next(minIndex, maxIndex);
+
+            _hiddenWord = File.ReadLines(_pathToTextFile)
+            .ElementAtOrDefault(hiddenWordIndex);
+        }
+
+        private static void GenerateVisibeWord()
+        {
+            VisibleWord = new string('_', _hiddenWord.Length);
+        }
+
+        public static string GetHiddenWord()
+        {
+            if (Player.IsPlayerLose || IsWordSolved)
+                return _hiddenWord;
+            else
+                return "Ошибка (Для отображения загаданного слова Вам необходимо проиграть/выиграть)";
+        }
+
+    }
+}
---
+++ HangMan/Player.cs
@@ -0,0 +1,30 @@
+﻿namespace HangMan
+{
+    public static class Player
+    {
+        // 6 mistakes, the player loses
+        private const int _maxNumberOfMistakes = 5;
+        public static int Mistakes { get; private set; } = 0;
+        public static List<char> UsedLetters { get; private set; } = new();
+        public static bool IsPlayerLose
+        {
+            get
+            {
+                return Mistakes > _maxNumberOfMistakes;
+            }
+        }
+        public static void AddMistake()
+        {
+            Mistakes++;
+        }
+
+        public static void Reset()
+        {
+            if (GuessWord.IsWordSolved || IsPlayerLose)
+            {
+                Mistakes = 0;
+                UsedLetters.Clear();
+            }            
+        }
+    }
+}
